7fa345480c119c84d560dad1b15a90a270adc657
qbq <chainz@foxmail.com>
2022-01-15 21:38:57 +0800

    commit

---
+++ workbench/interview/src/main/java/org/example/interview/leetcode/Q0005.java
@@ -0,0 +1,26 @@
+package org.example.interview.leetcode;
+
+import org.example.interview.annotations.LeetCodeMiddle;
+import org.example.interview.annotations.Tag;
+
+/**
+ * @author qbq
+ * @date 2022/1/14 4:19 PM
+ */
+public class Q0005 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 给你一个字符串 s，找到 s 中最长的回文子串。
+     */
+    //可以使用 动态规划 或者 双指针
+    @LeetCodeMiddle({Tag.string, Tag.dynamic_programing})
+    class Solution {
+        public String longestPalindrome(String s) {
+            return null;
+        }
+    }
+}
