1cf495200097a94e922170afe283ec1dad656b57
qbq <chainz@foxmail.com>
2022-01-29 17:25:47 +0800

    Create Q0020.java

---
+++ workbench/interview/src/main/java/org/example/interview/leetcode/Q0020.java
@@ -0,0 +1,33 @@
+package org.example.interview.leetcode;
+
+import org.example.interview.annotations.LeetCodeEasy;
+import org.example.interview.annotations.LeetCodeMiddle;
+import org.example.interview.annotations.Tag;
+
+/**
+ * @author qbq
+ * @date 2022/1/29 5:15 PM
+ */
+public class Q0020 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 20. 有效的括号
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     * <p>
+     * 有效字符串需满足：
+     * <p>
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     */
+    @LeetCodeEasy({Tag.stack, Tag.string})
+    static class Solution {
+        public boolean isValid(String s) {
+            return false;
+        }
+    }
+
+}
